d3aae4aa10496314e7114a228a2abcd19136e962
Noah H <nhopkins@kth.se>
2020-11-16 19:16:27 +0100

    Add project files to repo

---
+++ main/2_retrieve_sec_from_fra.py
@@ -0,0 +1,46 @@
+# This script retrieves the PDB ID, sequence and dot-bracket structures from FRABASE
+# for the PDB and BMRB matched IDs
+
+import urllib2
+import time
+
+inpf1=open('./bmrb_pdb_15N_20.txt')
+
+pdb_list = []
+
+for i in inpf1.readlines():
+    line = i.split()
+    pdb_list.append(line[1])
+
+
+for j in range(len(pdb_list)):
+
+    url= 'http://rnafrabase.cs.put.poznan.pl/?act=pdbdetails&id='+pdb_list[j]
+
+    response = urllib2.urlopen(url)
+    source = response.read()
+    source_lines = source.splitlines()
+    statements = source_lines[143].split()
+
+    hold_list = []
+
+    for k in range(len(statements)):
+
+        statement = statements[k]
+
+        if statement.find('((')!=-1 and statement.find('))')!=-1:
+            hold_list.append(statement)
+
+    if len(hold_list) > 0:
+        low_e_sec_struc_string = hold_list[0]
+
+        br = [i for i in range(len(low_e_sec_struc_string)) if low_e_sec_struc_string.startswith('<br>', i)]
+
+        sequence = low_e_sec_struc_string[(br[0]+4):(br[1])]
+        sec_struc = low_e_sec_struc_string[(br[1]+4):low_e_sec_struc_string.find('<',(br[1]+4))]
+
+        response.close()
+
+        print pdb_list[j]+" "+sequence+" "+sec_struc
+
+    time.sleep(1)

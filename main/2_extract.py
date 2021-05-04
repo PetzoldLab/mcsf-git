b688e1ce490013a5c0b4018a2bdd3c23580c620b
magdalenariad <magdalena.riad@ki.se>
2021-05-04 23:24:47 +0200

    Update the database to v.5.0 and bug fixes

---
+++ main/2_extract.py
@@ -0,0 +1,316 @@
+# ---------------------------------------------------------------------#
+# Script for extracting chemical shift data from the RNA CS database   #
+# CSF_DB_vX.X.db. The script requires input lists described below. It  #
+# will generate an output file "nuclei.txt" with the following struct: #
+#                                                                      #
+# Columns:                                                             #
+#   PDB ID, BMRB ID, residue ID, base, base before, base after,        #
+#   partner base, feature, nuclei type, chemical shift                 #
+#                                                                      #
+# Appearance:                                                          #
+#   2JTP 15417 1 G N G C s H1 12.0                                     #
+#                                                                      #
+# Dependencies:                                                        #
+#   forgi                                                              #
+#                                                                      #
+# Install forgi (any version, script initially written for version 1.1)#
+#   https://viennarna.github.io/forgi/                                 #
+#   https://www.tbi.univie.ac.at/RNA/documentation.html                #
+#                                                                      #
+# Authors: Hampus May-18, Noah Nov-20                                  #
+# ---------------------------------------------------------------------#
+
+import os
+import sqlite3
+import forgi
+from forgi.graph import bulge_graph as cgb
+
+# Input file 1: structures_imino_20.txt contains PDB id, Put in 'lists/'
+# Sequence and lowest energy secondary structure from FRABASE:
+# http://rnafrabase.cs.put.poznan.pl
+inpf1 = "structures_imino_20.txt"
+
+# Input file 2: bmrb_pdb_15N_20.txt contains a list of BMRB ID:s and the
+# corresponding PDB ID:s, extracted from NMR star files (with
+# bmrb2pdb.py in the "main" directory). Put in 'lists/'.
+# Download BMRB identities from:
+# https://bmrb.io/search/query_grid/query_1_20.html
+inpf2 = "BMRB_match_PDB_210419.txt"
+
+# Input Database (put in 'db/'):
+mcsf_db_path = "CSF_DB_v5.0.db"
+
+# Nuclei of interest
+# interesting_nuclei = ["H6","H8","H2","C6","C8","C2"]
+interesting_nuclei = ["H1", "H3", "N1", "N3"]
+
+# Output file:
+output_path = "nuclei_imino_20.txt"
+
+# -------- RUN -------- #
+
+# Import data
+split_path = os.path.abspath(os.getcwd()).split(os.path.sep)
+src_path = os.path.sep
+for a in range(len(split_path[1:-1])):
+    src_path = src_path + split_path[a + 1] + os.path.sep
+    
+inpf1 = open(os.path.normpath(os.path.join(src_path, "lists", inpf1)))
+inpf2 = open(os.path.normpath(os.path.join(src_path, "lists", inpf2)))
+mcsf_db_path = os.path.normpath(os.path.join("db", mcsf_db_path))
+output_path = os.path.normpath(os.path.join("lists", output_path))
+
+# create lists to hold pdb id, sequence and dot_bracket structures
+pdb_list, seq_list, dot_brack_list = [], [], []
+for i in inpf1.readlines():
+    line = i.split()
+    pdb_list.append(line[0])
+    seq_list.append(line[1])
+    dot_brack_list.append(line[2])
+
+# dictionary to hold bmrb ids and corresponding pdb ids
+bmrb_pdb = list(inpf2.readlines())
+d_bmrb_pdb = {}
+for j in bmrb_pdb:
+    pairs = j.split()
+    d_bmrb_pdb[pairs[0]] = pairs[1]
+
+# capitalize all the nucleotides MR
+for number in range(len(seq_list)):
+    seq_list[number] = seq_list[number].upper()
+
+# connect to sql database
+conn = sqlite3.connect(src_path + mcsf_db_path)
+c = conn.cursor()
+
+# select all from database table 'entry'
+c.execute("SELECT * FROM entry")
+entry_table = c.fetchall()
+
+# put bmrb id of wanted pdb structure in bmrb_id list
+bmrb_ids = []
+for row in entry_table:
+    if str(row[0]) in d_bmrb_pdb.keys():
+        if d_bmrb_pdb[str(row[0])] in pdb_list:
+            bmrb_ids.append(row[0])
+
+# print out bmrb and pdb ids
+print(27*"-" + "\nFound BMRB IDs in DB for:\n" + "-"*27 +
+      "\nBMRB    PDB")
+
+for bmrb_id in bmrb_ids:
+    print(str(bmrb_id) + " = " + d_bmrb_pdb[str(bmrb_id)])
+
+print("\nSummary:\nFound BMRB ID for " + str(len(bmrb_ids)) +
+      " out of " + str(len(pdb_list)) + " PDB structures\n")
+
+# collect all nuclei associated with these bmrb/pdb structures
+extr_seqs = []
+for i in range(len(bmrb_ids)):
+    
+    # to hold sequence and base letters
+    sequence = []
+    resid = 0
+    dummy_str = ""
+    
+    # for each individual structure
+    c.execute("SELECT * FROM cs WHERE BMRB_entry_ID=(?)",
+              (bmrb_ids[i],))
+    cs_table = c.fetchall()
+    
+    # extract sequence from returned bmrb entities
+    for row in cs_table:
+        if row[3] != resid:
+            sequence.append(row[4])
+            resid = int(row[3])
+    
+    # put all extracted sequences in "extr_seqs"
+    for letter in sequence:
+        dummy_str = dummy_str + letter
+    extr_seqs.append(dummy_str)
+
+conn.commit()
+conn.close()
+
+# check bmrb vs. fra base
+print(37*"-" + "\nCheck match between BMRB and FRAbase:\n" + 37*"-" +
+      "\n")
+
+# Control that extracted seqs from bmrb match with seqs from FRAbase
+filtered_bmrb_id = []  # bmrb ids of structures where sequences match
+for i in range(len(bmrb_ids)):
+    
+    ind1 = pdb_list.index(d_bmrb_pdb[str(bmrb_ids[i])])
+    print(str(bmrb_ids[i]) + " " + d_bmrb_pdb[str(bmrb_ids[i])])
+    print("From FRA:   " + seq_list[ind1])
+    
+    if seq_list[ind1] == extr_seqs[i]:
+        print("From BMRB:  " + extr_seqs[i] + "         OK!!!")
+        filtered_bmrb_id.append(bmrb_ids[i])
+    
+    else:
+        print("From BMRB:  " + extr_seqs[i] + "       OH NO!!!")
+    
+    print("            " + dot_brack_list[ind1] + "\n")
+
+print("Summary:\n" + str(len(filtered_bmrb_id)) +
+      " sequences OK out of " + str(len(bmrb_ids)) + "\n")
+
+# connect to database again
+conn = sqlite3.connect(src_path + mcsf_db_path)
+c = conn.cursor()
+
+# Collect all nuclei associated with the filtered bmrb/pdb structures:
+
+col_names = ("\nPDB\t\t\t"
+             "BMRB\t\t"
+             "Res\t\t"
+             "Base\t"
+             "BF\t\t"
+             "AF\t\t"
+             "Pair\t"
+             "feat\t"
+             "nuc\t\t"
+             "cs")
+
+print(27*"-" + "\nChosen nuclei of interest:\n" + "-"*27 + col_names)
+
+# save output to file
+text_file = open(src_path + output_path, "w")
+
+for i in range(len(filtered_bmrb_id)):
+    
+    # for each individual structure
+    c.execute("SELECT * FROM cs WHERE BMRB_entry_ID=(?)",
+              (filtered_bmrb_id[i],))
+    cs_table = c.fetchall()
+    
+    for nuclei_data in cs_table:
+    
+        residue_id  = nuclei_data[3]
+        nt_type     = nuclei_data[4]
+        nuc_type    = nuclei_data[5]
+        cs_val      = nuclei_data[6]
+        
+        fra_seq = seq_list[
+            pdb_list.index(d_bmrb_pdb[str(filtered_bmrb_id[i])])
+        ]
+        dot_brack = dot_brack_list[
+            pdb_list.index(d_bmrb_pdb[str(filtered_bmrb_id[i])])
+        ]
+        
+        # if forgi 1.x
+        if forgi.__version__.startswith("1"):
+            bg = cgb.BulgeGraph()
+            bg.from_dotbracket(dot_brack)
+            es = bg.to_element_string()
+        # if forgi 2.x
+        else:
+            bg = forgi.graph.bulge_graph.BulgeGraph
+            bg = bg.from_dotbracket(dot_brack)
+            es = bg.to_element_string()
+        
+        # define base pairing partner
+        partner = ""
+        if bg.pairing_partner(residue_id) is None:
+            partner = "N"
+        else:
+            partner = fra_seq[bg.pairing_partner(residue_id) - 1]
+
+        # If first nucleotide in a sequence
+        if nuc_type in interesting_nuclei and residue_id == 1:
+            
+            # PDB ID, BMRB ID, Residue ID, Base type, Base before
+            # Base after, partner base, feature, Nuc type, CS value
+            print(
+                d_bmrb_pdb[str(filtered_bmrb_id[i])] + "\t\t" +
+                str(filtered_bmrb_id[i]) + "\t\t" +
+                str(residue_id) + "\t\t" +
+                nt_type + "\t\t" + "N" + "\t\t" +
+                fra_seq[1] + "\t\t" +
+                partner + "\t\t" +
+                es[0] + "\t\t" +
+                nuc_type + "\t\t" +
+                str(cs_val)
+            )
+            
+            text_file.write(
+                d_bmrb_pdb[str(filtered_bmrb_id[i])] + " " +
+                str(filtered_bmrb_id[i]) + " " +
+                str(residue_id) + " " +
+                nt_type + " " +
+                "N" + " " +
+                fra_seq[1] + " " +
+                partner + " " +
+                es[0] + " " +
+                nuc_type + " " +
+                str(cs_val) + "\n"
+            )
+
+        # if not the first or the last nucleotide in sequence
+        elif (nuc_type in interesting_nuclei
+              and 1 < residue_id < len(fra_seq)):
+    
+            # PDB ID, BMRB ID, Residue ID, Base type, Base before
+            # Base after, partner base, feature, Nuc type, CS value
+            print(
+                d_bmrb_pdb[str(filtered_bmrb_id[i])] + "\t\t" +
+                str(filtered_bmrb_id[i]) + "\t\t" +
+                str(residue_id) + "\t\t" +
+                nt_type + "\t\t" +
+                fra_seq[residue_id - 2] + "\t\t" +
+                fra_seq[residue_id] + "\t\t" +
+                partner + "\t\t" +
+                es[residue_id - 1] + "\t\t" +
+                nuc_type + "\t\t" +
+                str(cs_val)
+            )
+            
+            text_file.write(
+                d_bmrb_pdb[str(filtered_bmrb_id[i])] + " " +
+                str(filtered_bmrb_id[i]) + " " +
+                str(residue_id) + " " +
+                nt_type + " " +
+                fra_seq[residue_id - 2] + " " +
+                fra_seq[residue_id] + " " +
+                partner + " " +
+                es[residue_id - 1] + " " +
+                nuc_type + " " +
+                str(cs_val) + "\n"
+            )
+
+        # if last nucleotide in sequence
+        elif (nuc_type in interesting_nuclei
+              and residue_id == len(fra_seq)):
+    
+            # PDB ID, BMRB ID, Residue ID, Base type, Base before
+            # Base after, partner base, feature, Nuc type, CS value
+            print(
+                d_bmrb_pdb[str(filtered_bmrb_id[i])] + "\t\t" +
+                str(filtered_bmrb_id[i]) + "\t\t" +
+                str(residue_id) + "\t\t" +
+                nt_type + "\t\t" +
+                fra_seq[residue_id - 2] + "\t\t" +
+                "N" + "\t\t" +
+                partner + "\t\t" +
+                es[residue_id - 1] + "\t\t" +
+                nuc_type + "\t\t" +
+                str(cs_val)
+            )
+            
+            text_file.write(
+                d_bmrb_pdb[str(filtered_bmrb_id[i])] + " " +
+                str(filtered_bmrb_id[i]) + " " +
+                str(residue_id) + " " +
+                nt_type + " " +
+                fra_seq[residue_id - 2] + " " +
+                "N" + " " +
+                partner + " " +
+                es[residue_id - 1] + " " +
+                nuc_type + " " +
+                str(cs_val) + "\n"
+            )
+
+text_file.close()
+conn.commit()
+conn.close()
---
+++ main/4AB_plot_cs_hsqc.py
@@ -0,0 +1,196 @@
+# Script to plot chemical shifts of imino protons and nitrogens from BMRB chemical shift data
+# Input "nuc_sparse.txt" and outputs .eps-plot
+# Author: Hampus May-18
+# Updated to be python2 and python3 compatible: Magdalena January 2021
+
+import numpy as np
+import matplotlib
+from platform import python_version
+
+if float(python_version()[0]) >= 3.0:
+    from matplotlib import pyplot as plt
+else:
+    matplotlib.use('TKAgg', warn=False, force=True)
+    from matplotlib import pyplot as plt
+
+plt.rc('xtick', labelsize=14)
+plt.rc('ytick', labelsize=14)
+
+# 2JTP 15417 1 G N G C s H1 12.0
+
+inpf = open("../lists/imino_nuc_sparse_21.txt", "r")
+lines = inpf.readlines()
+
+i = 0
+
+# UG
+ug_H3 = []
+ug_N3 = []
+
+# UA
+ua_H3 = []
+ua_N3 = []
+
+# GC
+gc_H1 = []
+gc_N1 = []
+
+# GU
+gu_H1 = []
+gu_N1 = []
+
+while i < (len(lines) - 1):
+
+    current_line = lines[i]
+    split_curr = current_line.split()
+
+    # 1269 2QH3 7404 2 G G A C s H1 N1 12.0 146.0
+
+    num, c_pdb_id, c_bmrbid, c_pos, c_base, c_basebf, c_basea, c_base_part, c_feat, c_H, c_N, c_Hcs, c_Ncs = split_curr
+
+    if (c_base == "U" and c_base_part == "G") and (c_feat == "s" and c_H == "H3"):
+
+        ug_H3.append(float(c_Hcs))
+        ug_N3.append(float(c_Ncs))
+
+    elif (c_base == "U" and c_base_part == "A") and (c_feat == "s" and c_H == "H3"):
+
+        ua_H3.append(float(c_Hcs))
+        ua_N3.append(float(c_Ncs))
+
+    elif (c_base == "G" and c_base_part == "C") and (c_feat == "s" and c_H == "H1"):
+
+        gc_H1.append(float(c_Hcs))
+        gc_N1.append(float(c_Ncs))
+
+    elif (c_base == "G" and c_base_part == "U") and (c_feat == "s" and c_H == "H1"):
+
+        gu_H1.append(float(c_Hcs))
+        gu_N1.append(float(c_Ncs))
+
+    i += 1
+
+inpf.close()
+
+# Average and std U-G
+avg_ug_H3 = round(np.average(np.asarray(ug_H3)), 3)
+avg_ug_N3 = round(np.average(np.asarray(ug_N3)), 3)
+
+std_ug_H3 = round(np.std(np.asarray(ug_H3)), 3)
+std_ug_N3 = round(np.std(np.asarray(ug_N3)), 3)
+
+# Average and std U-A
+
+avg_ua_H3 = round(np.average(np.asarray(ua_H3)), 3)
+avg_ua_N3 = round(np.average(np.asarray(ua_N3)), 3)
+
+std_ua_H3 = round(np.std(np.asarray(ua_H3)), 3)
+std_ua_N3 = round(np.std(np.asarray(ua_N3)), 3)
+
+# Average and std G-C
+avg_gc_H1 = round(np.average(np.asarray(gc_H1)), 3)
+avg_gc_N1 = round(np.average(np.asarray(gc_N1)), 3)
+
+std_gc_H1 = round(np.std(np.asarray(gc_H1)), 3)
+std_gc_N1 = round(np.std(np.asarray(gc_N1)), 3)
+
+# Average and std G-U
+avg_gu_H1 = round(np.average(np.asarray(gu_H1)), 3)
+avg_gu_N1 = round(np.average(np.asarray(gu_N1)), 3)
+
+std_gu_H1 = round(np.std(np.asarray(gu_H1)), 3)
+std_gu_N1 = round(np.std(np.asarray(gu_N1)), 3)
+
+# plot
+fig = plt.figure()
+params = {'legend.fontsize': 20,
+          'legend.handlelength': 2}
+plt.rcParams.update(params)
+plt.plot(ug_H3, ug_N3, "mo", markersize=6, zorder=1, label="UG n=" + str(len(ug_H3)))
+plt.errorbar(avg_ug_H3, avg_ug_N3, std_ug_N3, std_ug_H3, "kx", markersize=6)
+plt.plot(ua_H3, ua_N3, "bo", markersize=6, zorder=1, label="UA n=" + str(len(ua_H3)))
+plt.errorbar(avg_ua_H3, avg_ua_N3, std_ua_N3, std_ua_H3, "kx", markersize=6)
+plt.plot(gc_H1, gc_N1, "co", markersize=6, zorder=1, label="GC n=" + str(len(gc_H1)))
+plt.errorbar(avg_gc_H1, avg_gc_N1, std_gc_N1, std_gc_H1, "kx", markersize=6)
+plt.plot(gu_H1, gu_N1, "yo", markersize=6, zorder=1, label="GU n=" + str(len(gu_H1)))
+plt.errorbar(avg_gu_H1, avg_gu_N1, std_gu_N1, std_gu_H1, "kx", markersize=6)
+
+# Adding ellipses with 1 sd
+# GU
+gu_horizontal_radius = std_gu_H1
+gu_vertical_radius = std_gu_N1
+gu_x_center = avg_gu_H1
+gu_y_center = avg_gu_N1
+
+gu_x = np.linspace(0, 15, 1000)
+gu_y = np.linspace(135, 150, 1000)
+
+gu_X, gu_Y = np.meshgrid(gu_x, gu_y)
+
+gu_eqn = (((gu_X - gu_x_center) ** 2) / (gu_horizontal_radius ** 2)) + (
+            ((gu_Y - gu_y_center) ** 2) / (gu_vertical_radius ** 2))
+Z = 1
+
+plt.contour(gu_X, gu_Y, gu_eqn, [Z], colors="k", linewidths=1, linestyles="dashed")
+
+# GC
+gc_horizontal_radius = std_gc_H1
+gc_vertical_radius = std_gc_N1
+gc_x_center = avg_gc_H1
+gc_y_center = avg_gc_N1
+
+gc_x = np.linspace(0, 15, 1000)
+gc_y = np.linspace(135, 155, 1000)
+
+gc_X, gc_Y = np.meshgrid(gc_x, gc_y)
+
+gc_eqn = (((gc_X - gc_x_center) ** 2) / (gc_horizontal_radius ** 2)) + (((gc_Y - gc_y_center) ** 2) / (
+        gc_vertical_radius ** 2))
+Z = 1
+
+plt.contour(gc_X, gc_Y, gc_eqn, [Z], colors="k", linewidths=1, linestyles="dashed")
+
+# UG
+ug_horizontal_radius = std_ug_H3
+ug_vertical_radius = std_ug_N3
+ug_x_center = avg_ug_H3
+ug_y_center = avg_ug_N3
+
+ug_x = np.linspace(0, 15, 1000)
+ug_y = np.linspace(150, 165, 1000)
+
+ug_X, ug_Y = np.meshgrid(ug_x, ug_y)
+
+ug_eqn = (((ug_X - ug_x_center) ** 2) / (ug_horizontal_radius ** 2)) + (
+            ((ug_Y - ug_y_center) ** 2) / (ug_vertical_radius ** 2))
+Z = 1
+
+plt.contour(ug_X, ug_Y, ug_eqn, [Z], colors="k", linewidths=1, linestyles="dashed")
+
+# UA
+ua_horizontal_radius = std_ua_H3
+ua_vertical_radius = std_ua_N3
+ua_x_center = avg_ua_H3
+ua_y_center = avg_ua_N3
+
+ua_x = np.linspace(0, 15, 1000)
+ua_y = np.linspace(150, 165, 1000)
+
+ua_X, ua_Y = np.meshgrid(ua_x, ua_y)
+
+ua_eqn = (((ua_X - ua_x_center) ** 2) / (ua_horizontal_radius ** 2)) + (
+            ((ua_Y - ua_y_center) ** 2) / (ua_vertical_radius ** 2))
+Z = 1
+
+plt.contour(ua_X, ua_Y, ua_eqn, [Z], colors="k", linewidths=1, linestyles="dashed")
+
+plt.title("Imino chemical shifts", fontsize=20)
+plt.xlabel("1H (ppm)", fontsize=20)
+plt.ylabel("15N (ppm)", fontsize=20)
+plt.legend()
+
+plt.xlim(15.0, 10.0)
+plt.ylim(170.0, 135.0)
+plt.show()
+
+#fig.savefig("bmrb_imino_21.eps", format='eps')
